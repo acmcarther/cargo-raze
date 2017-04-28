ae6126152637e0eb1e30082915ba42d5092a5efe
Alex McArther <acmcarther@gmail.com>
2017-04-28 14:26:39 -0700

    Clean up some code cruft

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ extern crate cargo;
 #[macro_use]
 extern crate nom;
 extern crate rustc_serialize;
+extern crate itertools;
 
-use cargo::CargoError;
 use cargo::CliResult;
 use cargo::core::Dependency;
 use cargo::core::Package;
@@ -18,7 +18,6 @@ use cargo::util::ChainError;
 use cargo::util::Config;
 use cargo::util::ToUrl;
 use cargo::util::human;
-use std::cmp;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::env;
@@ -28,12 +27,6 @@ use std::fs::File;
 use std::path::Path;
 use std::str::FromStr;
 use std::str;
-
-#[derive(Debug)]
-pub struct DependencyWithPath {
-  pub dependency: Dependency,
-  pub path: String
-}
 
 /** Define parser for --overrides flag */
 fn isnt_plus(chr: char) -> bool { chr != '+' }
@@ -108,142 +101,162 @@ fn real_main(options: Options, config: &Config) -> CliResult {
                           /* frozen = */ false,
                           /* locked = */ false));
     let platform_triple = config.rustc()?.host.clone();
-    let registry_id = try!(load_registry(options.flag_host, &config));
-    try!(fs::metadata("Cargo.lock").chain_error(|| {
-      human("failed to find Cargo.lock. Please run `cargo generate-lockfile` first.")
-    }));
-    let lockfile = Path::new("Cargo.lock");
-    let manifest_path = lockfile.parent().unwrap().join("Cargo.toml");
-    let manifest = env::current_dir().unwrap().join(&manifest_path);
-    let ws = try!(Workspace::new(&manifest, config));
-    let specs = Packages::All.into_package_id_specs(&ws).chain_error(|| {
-      human("failed to find specs? whats a spec?")
-    })?;
-
     // TODO(acmcarther): Fix unwrap. I'm unwrapping here temporarily because Nom's err is hard to
     // convert to CargoError
-    let overrides = options.flag_overrides.as_ref()
-      .map(|f| parse_overrides(f).to_result().unwrap())
-      .unwrap_or(Vec::new());
-    let override_name_and_ver_to_path: HashMap<(String, String), String> = overrides.into_iter()
-      .map(|entry| ((entry.name, entry.version), entry.bazel_path))
-      .collect();
+    let override_name_and_ver_to_path: HashMap<(String, String), String> = {
+        options.flag_overrides.as_ref()
+            .map(|f| parse_overrides(f).to_result().unwrap())
+            .unwrap_or(Vec::new())
+            .into_iter()
+            .map(|entry| ((entry.name, entry.version), entry.bazel_path))
+            .collect()
+    };
 
-    let (packages, resolve) = ops::resolve_ws_precisely(
-            &ws,
-            None,
-            &[],
-            false,
-            false,
-            &specs).chain_error(|| {
-        human("failed to load pkg lockfile")
-    })?;
+    let (packages, resolve) = {
+        let lockfile = Path::new("Cargo.lock");
+        let manifest_path = lockfile.parent().unwrap().join("Cargo.toml");
+        let manifest = env::current_dir().unwrap().join(&manifest_path);
+        let ws = try!(Workspace::new(&manifest, config));
+        let specs = Packages::All.into_package_id_specs(&ws).chain_error(|| {
+          human("failed to find specs? whats a spec?")
+        })?;
 
-    let mut package_ids = resolve.iter()
-                     .filter(|id| *id.source_id() == registry_id)
-                     .cloned()
-                     .collect::<Vec<_>>();
-    package_ids.sort_by_key(|id| id.name().to_owned());
+        ops::resolve_ws_precisely(
+                &ws,
+                None,
+                &[],
+                false,
+                false,
+                &specs).chain_error(|| {
+            human("failed to load pkg lockfile")
+        })?
+    };
 
-    let mut max = HashMap::new();
-    for id in package_ids.iter() {
-        let max = max.entry(id.name()).or_insert(id.version());
-        *max = cmp::max(id.version(), *max)
-    }
+    let package_ids = {
+        let source_id_from_registry =
+          options.flag_host.map(|s| s.to_url().map(|url| SourceId::for_registry(&url)).map_err(human));
 
-    let mut raze_packages = Vec::new();
+        let registry_id = try!(source_id_from_registry.unwrap_or_else(|| SourceId::crates_io(config)));
+        try!(fs::metadata("Cargo.lock").chain_error(|| {
+          human("failed to find Cargo.lock. Please run `cargo generate-lockfile` first.")
+        }));
 
-    for id in package_ids.iter() {
-        // Skip generating new_crate_repository for overrides
-        if override_name_and_ver_to_path.contains_key(&(id.name().to_owned(), id.version().to_string())) {
-          continue
-        }
+        let mut package_ids = resolve.iter()
+            .filter(|id| *id.source_id() == registry_id)
+            .cloned()
+            .collect::<Vec<_>>();
+        package_ids.sort_by_key(|id| id.name().to_owned());
+        package_ids
+    };
 
-        raze_packages.push(RazePackage {
+    let raze_packages = package_ids.iter()
+      .filter(|id| !override_name_and_ver_to_path.contains_key(&(id.name().to_owned(), id.version().to_string())))
+      .map(|id| RazePackage {
           id: id.clone(),
           package: packages.get(id).unwrap().clone(),
           // TODO(acmcarther): This will break as of cargo commit 50f1c172
           features: resolve.features(id)
             .cloned()
-            .unwrap_or(HashSet::new()),
-        });
-    }
+            .unwrap_or(HashSet::new())})
+      .collect::<Vec<_>>();
 
-    // Verify that cargo-vendor has already vendored the dependencies
-    {
-        for raze_pkg in raze_packages.iter() {
-            let package_dir = format!("{}-{}/", raze_pkg.id.name(), raze_pkg.id.version());
-            try!(fs::metadata(&package_dir).chain_error(|| {
-                human(format!("failed to find {}. Please run `cargo vendor -x .` first.", package_dir))
-            }));
-        }
-    }
+    for pkg in raze_packages.iter() {
+        let &RazePackage {ref id, ref features, ref package, ..} = pkg;
+        let vendor_dir = format!("./{}-{}/", id.name(), id.version());
 
-    // Check for Build dependencies -- these will be lost on generation
-    {
-        let mut printed_warning = false;
-        for raze_pkg in raze_packages.iter() {
-            if raze_pkg.package.dependencies().iter().any(|dep| dep.kind() == Kind::Build) {
-                printed_warning = true;
-                println!("WARNING: Crate <{}-{}> appears to contain a Build dependency.",
-                    raze_pkg.id.name(), raze_pkg.id.version());
-
-            }
-        }
-        if printed_warning {
-            println!("WARNING: You will probably need to override Build dependent crates with the --override flag and provide a custom BUILD rule.");
-        }
-    }
-    {
-        for raze_pkg in raze_packages.iter() {
-            let &RazePackage {ref id, ref features, ref package, ..} = raze_pkg;
-            let vendor_dir = format!("./{}-{}/", id.name(), id.version());
-            let vendor_path = Path::new(&vendor_dir);
-            let dependencies_by_name = package.dependencies().iter().cloned()
-                .map(|dep| (dep.name().to_owned(), dep))
-                .collect::<HashMap<String, Dependency>>();
-            let dependencies = resolve.deps(id).into_iter()
-              .map(|dep| {
-                assert!(dependencies_by_name.contains_key(dep.name()));
-                let overriid = override_name_and_ver_to_path.get(&(dep.name().to_owned(), dep.version().to_string()));
-                let path = match overriid {
-                  Some(override_path) => override_path.clone(),
-                  None => format!("//{crate_full_name}:{sanitized_name}",
-                    crate_full_name=format!("{}-{}", dep.name().replace("-", "_"), dep.version()),
-                    sanitized_name=dep.name().replace("-", "_"))
-                };
-                DependencyWithPath {
-                  dependency: dependencies_by_name.get(dep.name())
-                    .expect("Dependencies from 'resolve' object should also be contained in 'package'")
-                    .clone(),
-                  path: path
-                }
-              })
-              .collect::<Vec<_>>();
-            let build_file_content = generate_build_file(
-                                      id.name(),
-                                      &dependencies,
-                                      &features,
-                                      &platform_triple);
-            let build_file_path = vendor_path.join("BUILD");
-            try!(File::create(&build_file_path).and_then(|mut f| f.write_all(build_file_content.as_bytes())).chain_error(|| {
-                human(format!("failed to create: `{}`", build_file_path.display()))
-            }));
-        }
-        let workspace_path = Path::new("WORKSPACE");
-        try!(File::create(&workspace_path).chain_error(|| {
-            human(format!("failed to create: `{}`", workspace_path.display()))
+        try!(fs::metadata(&vendor_dir).chain_error(|| {
+            human(format!("failed to find {}. Please run `cargo vendor -x .` first.", vendor_dir))
         }));
+
+        let vendor_path = Path::new(&vendor_dir);
+
+        if package.dependencies().iter().any(|dep| dep.kind() == Kind::Build) {
+            println!("WARNING: Crate <{}-{}> appears to contain a Build dependency.",
+                id.name(), id.version());
+        }
+
+        let package_dependencies_by_name = package.dependencies().iter().cloned()
+            .map(|dep| (dep.name().to_owned(), dep))
+            .collect::<HashMap<String, Dependency>>();
+
+        let resolved_dependencies_by_name = resolve.deps(id).into_iter()
+            .map(|dep| (dep.name().to_owned(), dep.clone()))
+            .collect::<HashMap<String, PackageId>>();
+
+        let all_dependencies = package_dependencies_by_name.keys().cloned()
+            .chain(resolved_dependencies_by_name.keys().cloned())
+            .collect::<HashSet<_>>();
+
+        let mut bazel_dependency_strs = Vec::new();
+        for dependency_name in all_dependencies.iter() {
+            if !resolved_dependencies_by_name.contains_key(dependency_name) {
+                continue
+            }
+            let resolved_dependency = resolved_dependencies_by_name.get(dependency_name).unwrap();
+            assert!(package_dependencies_by_name.contains_key(dependency_name));
+            let package_dependency = package_dependencies_by_name.get(dependency_name).unwrap();
+
+            let dependency_version = resolved_dependency.version();
+            let dependency_override = override_name_and_ver_to_path
+                .get(&(dependency_name.to_owned(), dependency_version.to_string()));
+
+            if package_dependency.kind() != Kind::Normal {
+                println!("WARNING: Crate <{}> is dropping <{:?}> dependency <{}>",
+                         id.name(), package_dependency.kind(), dependency_name);
+                continue
+            }
+
+            let platform_requires_dependency = package_dependency.platform()
+              .map(|p| p.matches(&platform_triple, Some(&generic_linux_cfgs())))
+              .unwrap_or(true);
+            if platform_requires_dependency {
+                println!("INFO: Crate <{}> is dropping <{}> because it is not for this plaform.",
+                         id.name(), dependency_name);
+                println!("INFO: Dependency <{}>'s target specification is <{:?}>",
+                         dependency_name, package_dependency.platform().unwrap());
+                continue
+            }
+
+            let bazel_dependency_path = match dependency_override {
+                Some(override_path) => override_path.clone(),
+                None => format!("//{sanitized_name}-{crate_version}:{sanitized_name}",
+                  sanitized_name=dependency_name.replace("-", "_"),
+                  crate_version=resolved_dependency.version())
+            };
+            bazel_dependency_strs.push(bazel_dependency_path);
+        }
+
+        let sanitized_crate_name = id.name().replace("-", "_");
+        let mut comma_separated_deps = bazel_dependency_strs.into_iter()
+            .map(|dep_str| format!("        \"{}\",\n", dep_str))
+            .collect::<Vec<String>>();
+        comma_separated_deps.sort();
+        let mut comma_separated_features = features
+            .iter()
+            .map(|f| format!("        \"{}\",\n", f))
+            .collect::<Vec<String>>();
+        comma_separated_features.sort();
+
+        let comma_separated_deps_str =
+            comma_separated_deps.into_iter().collect::<String>();
+        let comma_separated_features_str =
+            comma_separated_features.into_iter().collect::<String>();
+
+        let build_file_content = generate_build_file(
+            &sanitized_crate_name,
+            &comma_separated_deps_str,
+            &comma_separated_features_str);
+
+        let build_file_path = vendor_path.join("BUILD");
+        try!(File::create(&build_file_path)
+            .and_then(|mut f| f.write_all(build_file_content.as_bytes()))
+            .chain_error(|| human(format!("failed to create: `{}`", build_file_path.display()))));
     }
+    let workspace_path = Path::new("WORKSPACE");
+    try!(File::create(&workspace_path)
+        .chain_error(|| human(format!("failed to create: `{}`", workspace_path.display()))));
 
     Ok(())
-}
-
-fn load_registry(flag_host: Option<String>, config: &Config) -> Result<SourceId, Box<CargoError>> {
-    let source_id_from_registry =
-      flag_host.map(|s| s.to_url().map(|url| SourceId::for_registry(&url)).map_err(human));
-
-    source_id_from_registry.unwrap_or_else(|| SourceId::crates_io(config))
 }
 
 fn generic_linux_cfgs() -> Vec<Cfg> {
@@ -275,38 +288,14 @@ unix"#;
 }
 
 
-pub fn generate_build_file(crate_name: &str, dependencies: &Vec<DependencyWithPath>, feature_strings: &HashSet<String>, platform_triple: &str) -> String {
-    let mut unused_dependencies_with_reason: Vec<String> = Vec::new();
-    let mut dependency_strs = Vec::new();
-
-    for dep in dependencies.iter() {
-        let &DependencyWithPath { ref dependency, ref path } = dep;
-        if dependency.kind() != Kind::Normal {
-            unused_dependencies_with_reason.push(
-              format!("Dependency <{}> is <{:?}> dependency", path.clone(), dependency.kind()));
-            continue
-        }
-
-        if dependency.platform().map(|p| !p.matches(platform_triple, Some(&generic_linux_cfgs()))).unwrap_or(false) {
-            unused_dependencies_with_reason.push(
-              format!("Dependency <{}> is for alternate platform: <{}>", path.clone(), dependency.platform().unwrap()));
-            continue
-        }
-
-        dependency_strs.push(path);
-    }
-
-    // Make output "stable" by sorting
-    dependency_strs.sort();
-    unused_dependencies_with_reason.sort();
-
-    let unused_dependencies_str = unused_dependencies_with_reason.into_iter().map(|msg| format!("- {}\n", msg)).collect::<String>();
+pub fn generate_build_file(
+        sanitized_crate_name: &str,
+        comma_separated_deps: &String,
+        comma_separated_features: &String) -> String {
     format!(r#"'''
 WARNING: THIS IS GENERATED CODE!
 DO NOT MODIFY!
 Instead, rerun raze with different arguments.
-
-{notes}
 '''
 package(default_visibility = ["//visibility:public"])
 
@@ -328,22 +317,7 @@ rust_library(
     crate_features = [
 {comma_separated_features}    ],
 )"#,
-    sanitized_crate_name = crate_name.replace("-", "_"),
-    comma_separated_deps = dependency_strs.into_iter()
-        .map(|dep_str| format!("        \"{}\",\n", dep_str))
-        .collect::<String>(),
-    comma_separated_features = bazelize_features(feature_strings),
-    notes = format!(
-"Unused dependencies from cargo: [
-{unused_dependencies}]"
-, unused_dependencies=unused_dependencies_str))
-}
-
-fn bazelize_features(features: &HashSet<String>) -> String {
-    let mut feature_strs = features
-      .iter()
-      .map(|f| format!("        \"{}\",\n", f))
-      .collect::<Vec<String>>();
-    feature_strs.sort();
-    return feature_strs.into_iter().collect::<String>();
+        sanitized_crate_name=sanitized_crate_name,
+        comma_separated_deps=comma_separated_deps,
+        comma_separated_features=comma_separated_features)
 }
